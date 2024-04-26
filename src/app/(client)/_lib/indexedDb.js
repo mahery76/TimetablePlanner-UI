045159a0a4b81fa4541868cd0d3c06deddda4d02
Mahery Antonio <randrianjafymahery@gmail.com>
2024-04-26 15:43:47 +0300

    chore: bulk data to indexedDb

--- src/app/(client)/_lib/indexedDb.js
+++ src/app/(client)/_lib/indexedDb.js
@@ -1,21 +1,41 @@
 import Dexie from "dexie";
-import { teacherAvailabilitiesData, teachersData, timeslotsData } from "./indexedDbDefaultdata";
+import {
+  classroomsData,
+  core_class_refsData,
+  coursesData,
+  groupsData,
+  majorsData,
+  teacherAvailabilitiesData,
+  teachersData,
+  timeslotsData,
+} from "./indexedDbDefaultdata";
 
 export const db = new Dexie("coolplannerdb");
 db.version(1).stores({
   teachers: "++teacher_id, teacher_name, teacher_title, teacher_profil_pic",
   timeslots: "++timeslot_id, timeslot_value",
-  teacher_availabilities: "++teacher_availability_id, availability_date, teacher_id, timeslot_id"
+  teacher_availabilities:
+    "++teacher_availability_id, availability_date, teacher_id, timeslot_id",
+  majors: "++major_id, major_name",
+  groups: "++group_id, group_size, group_name, major_id",
+  courses: "++course_id, course_name, major_id",
+  core_class_refs: "++core_class_ref_id, core_class_ref_name",
+  classrooms: "++classroom_id, classroom_name, classroom_capacity",
+  activities:
+    "++activity_id, activity_hours, activity_remaining_hours, course_id, teacher_id, class_id, room_id, core_classe_id",
 });
 
-db.on('populate', () => {
+db.on("populate", () => {
   db.teachers.bulkAdd(teachersData);
   db.timeslots.bulkAdd(timeslotsData);
   db.teacher_availabilities.bulkAdd(teacherAvailabilitiesData);
+  db.majors.bulkAdd(majorsData);
+  db.groups.bulkAdd(groupsData);
+  db.courses.bulkAdd(coursesData);
+  db.core_class_refs.bulkAdd(core_class_refsData);
+  db.classrooms.bulkAdd(classroomsData);
 });
 
-db.open().catch(function(error){
-    console.error("Open failed: " + error)
-})
-
-
+db.open().catch(function (error) {
+  console.error("Open failed: " + error);
+});
